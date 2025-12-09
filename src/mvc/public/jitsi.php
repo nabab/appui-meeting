1bd6fc5d55ea6c89a9cac5929e8f7b90aecc3d70
nabab <thomas.nabet@gmail.com>
2025-12-09 23:14:10 +0100

    Comments

--- src/mvc/public/jitsi.php
+++ src/mvc/public/jitsi.php
@@ -1,10 +1,5 @@
 <?php
-/*
- * Describe what it does!
- *
- * @var bbn\Mvc\Controller $ctrl 
- *
- */
+/** @var bbn\Mvc\Controller $ctrl */
 if (defined('BBN_MEET_DOMAIN')) {
   $ctrl->combo(_("Meeting BBN"), ['domain' => BBN_MEET_DOMAIN]);
 }
